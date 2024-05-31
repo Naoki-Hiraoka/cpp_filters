374c847e4aa58d95f6e591c666eac6325413f6cb
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2024-05-31 19:32:33 +0900

    add cpp_filters.h

---
+++ include/cpp_filters/cpp_filters.h
@@ -0,0 +1,8 @@
+#ifndef CPP_FILTERS_CPP_FILTERS_H
+#define CPP_FILTERS_CPP_FILTERS_H
+
+#include <cpp_filters/FirstOrderLowPassFilter.h>
+#include <cpp_filters/IIRFilter.h>
+#include <cpp_filters/TwoPointInterpolator.h>
+
+#endif
